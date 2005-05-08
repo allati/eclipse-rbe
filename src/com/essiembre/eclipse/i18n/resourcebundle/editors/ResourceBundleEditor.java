9c5419677073013a9e72e88253cea27a9856e378
essiembre <essiembre>
2005-05-08 04:15:48 +0000

    Added support for Eclipse "nl" directory structure.

--- src/com/essiembre/eclipse/i18n/resourcebundle/editors/ResourceBundleEditor.java
+++ src/com/essiembre/eclipse/i18n/resourcebundle/editors/ResourceBundleEditor.java
@@ -26,7 +26,10 @@ import java.util.List;
 import java.util.Locale;
 import java.util.StringTokenizer;
 
+import org.eclipse.core.internal.resources.Folder;
+import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
@@ -156,76 +159,14 @@ public class ResourceBundleEditor extends MultiPageEditorPart {
             closeIfAreadyOpen(site);
             // Create editors
             bundles = new Bundles();
-            IResource[] resources = null;
             try {
-                resources = file.getParent().members();
+                loadNLBundles(site, file);
             } catch (CoreException e) {
-                throw new PartInitException(
-                        "Can't initialize resource bundle editor.", e);
+                e.printStackTrace();
+                throw new PartInitException("Can't initialize editor.", e);
             }
-            for (int i = 0; i < resources.length; i++) {
-                IResource resource = resources[i];
-                String regex = "^(" + bundleName + ")"
-                        + "((_[a-z]{2,3})|(_[a-z]{2,3}_[A-Z]{2})"
-                        + "|(_[a-z]{2,3}_[A-Z]{2}_\\w*))?(\\."
-                        + file.getFileExtension() + ")$";
-                String resourceName = resource.getName();
-                if (resource instanceof IFile && resourceName.matches(regex)) {
-                    Bundle bundle = new Bundle();
-                    
-                    // Build local title
-                    String localeText = resourceName.replaceFirst(regex, "$2");
-                    StringTokenizer tokens = 
-                        new StringTokenizer(localeText, "_");
-                    List localeSections = new ArrayList();
-                    while (tokens.hasMoreTokens()) {
-                        localeSections.add(tokens.nextToken());
-                    }
-                    String title = null;
-                    Locale locale = null;
-                    switch (localeSections.size()) {
-                    case 0:
-                        title = ResourceBundlePlugin.getResourceString(
-                                "editor.default");
-                        break;
-                    case 1:
-                        locale = new Locale((String) localeSections.get(0));
-                        title = locale.getDisplayName();
-                        break;
-                    case 2:
-                        locale = new Locale(
-                                (String) localeSections.get(0),
-                                (String) localeSections.get(1));
-                        title = locale.getDisplayName();
-                        break;
-                    case 3:
-                        locale = new Locale(
-                                (String) localeSections.get(0),
-                                (String) localeSections.get(1),
-                                (String) localeSections.get(2));
-                        title = locale.getDisplayName();
-                        break;
-                    default:
-                        break;
-                    }
-                    bundle.setLocale(locale);
-                    bundle.setTitle(title);
-                    
-                    IEditorInput newEditorInput = 
-                            new FileEditorInput((IFile) resource);
-                    TextEditor textEditor = null;
-                    try {
-                        // Use PropertiesFileEditor if available
-                        textEditor = (TextEditor) Class.forName(
-                                PROPERTIES_EDITOR_CLASS_NAME).newInstance();
-                    } catch (Exception e) {
-                        // Use default editor otherwise
-                        textEditor = new TextEditor();
-                    }
-                    textEditor.init(site, newEditorInput);
-                    bundle.setEditor(textEditor);
-                    bundles.addBundle(bundle);
-                }
+            if (bundles.count() == 0) {
+                loadStandardBundles(site, file);
             }
             bundles.refreshData();
             super.init(site, editorInput);
@@ -252,6 +193,181 @@ public class ResourceBundleEditor extends MultiPageEditorPart {
         }
     }
 
+    /**
+     * Load resource bundles the standard way, i.e., a set of *.properties 
+     * files in the same directory, with a locale representation in the 
+     * file names.
+     * @param site IEditorSite instance
+     * @param file IFile instance
+     * @throws PartInitException problem loading bundles
+     */
+    private void loadStandardBundles(IEditorSite site, IFile file)
+            throws PartInitException {
+        IResource[] resources = null;
+        try {
+            resources = file.getParent().members();
+        } catch (CoreException e) {
+            throw new PartInitException(
+                    "Can't initialize resource bundle editor.", e);
+        }
+        for (int i = 0; i < resources.length; i++) {
+            IResource resource = resources[i];
+            String regex = "^(" + bundleName + ")"
+                    + "((_[a-z]{2,3})|(_[a-z]{2,3}_[A-Z]{2})"
+                    + "|(_[a-z]{2,3}_[A-Z]{2}_\\w*))?(\\."
+                    + file.getFileExtension() + ")$";
+            String resourceName = resource.getName();
+            if (resource instanceof IFile && resourceName.matches(regex)) {
+                
+                // Build local title
+                String localeText = resourceName.replaceFirst(regex, "$2");
+                StringTokenizer tokens = 
+                    new StringTokenizer(localeText, "_");
+                List localeSections = new ArrayList();
+                while (tokens.hasMoreTokens()) {
+                    localeSections.add(tokens.nextToken());
+                }
+                switch (localeSections.size()) {
+                case 0:
+                    loadBundle(site, resource, null, null, null);
+                    break;
+                case 1:
+                    loadBundle(site, resource, 
+                            (String) localeSections.get(0), null, null);
+                    break;
+                case 2:
+                    loadBundle(site, resource, 
+                            (String) localeSections.get(0),
+                            (String) localeSections.get(1), null);
+                    break;
+                case 3:
+                    loadBundle(site, resource, 
+                            (String) localeSections.get(0),
+                            (String) localeSections.get(1),
+                            (String) localeSections.get(2));
+                    break;
+                default:
+                    break;
+                }
+            }
+        }
+    }
+    
+    /**
+     * Loads resource bundles laid out the recommended way when 
+     * internationalizing Eclipse plugins.
+     * @param site IEditorSite instance
+     * @param file IFile instance
+     * @throws PartInitException problem loading bundles
+     * @throws CoreException problem loading bundles
+     */
+    private void loadNLBundles(IEditorSite site, IFile file)
+            throws PartInitException, CoreException {
+        String filename = file.getName();
+        
+        // Locate "nl" directory (if any)
+        IContainer container = file.getParent();
+        IResource nlDir = null;
+        while (container != null 
+                && nlDir == null || !(nlDir instanceof Folder)) {
+            nlDir = container.findMember("nl");
+            container = container.getParent();
+        }
+        
+        // Load properties files in "nl" structure.
+        if (nlDir != null && nlDir instanceof Folder) {
+            // Load root file, if exists.
+            IResource resource = nlDir.getParent().findMember(filename);
+            loadBundle(site, resource, null, null, null);
+
+            // Load "language" matching files in "nl" tree.
+            IResource[] langResources = ((Folder) nlDir).members();
+            for (int i = 0; i < langResources.length; i++) {
+                String language = null;
+                IResource langResource = langResources[i];
+                if (langResource instanceof IFolder) {
+                    IFolder langFolder = (IFolder) langResource;
+                    language = langFolder.getName();
+                    loadBundle(site, langFolder.findMember(filename),
+                            language, null, null);
+
+                    // Load "country" matching files in "nl" tree.
+                    String country = null;
+                    IResource[] cntryResources = langFolder.members();
+                    for (int j = 0; j < cntryResources.length; j++) {
+                        IResource cntryResource = cntryResources[j];
+                        if (cntryResource instanceof IFolder) {
+                            IFolder cntryFolder = (IFolder) cntryResource;
+                            country = cntryFolder.getName();
+                            loadBundle(site, cntryFolder.findMember(filename),
+                                    language, country, null);
+                            
+                            // Load "variant" matching files in "nl" tree.
+                            IResource[] vrntResources = cntryFolder.members();
+                            for (int k = 0; k < vrntResources.length; k++) {
+                                IResource vrntResource = vrntResources[k];
+                                if (vrntResource instanceof IFolder) {
+                                    IFolder vrntFolder = (IFolder) vrntResource;
+                                    loadBundle(site, 
+                                            vrntFolder.findMember(filename),
+                                            language, country, 
+                                            vrntFolder.getName());
+                                }
+                            }
+                        }
+                    }                        
+                }
+            }
+        }
+    }
+
+    /**
+     * Loads a resource bundle.
+     * @param site IEditorSite instance
+     * @param resource the IResource to load, provided it is a file.
+     * @param lang ISO language code
+     * @param country ISO country code
+     * @param variant locale variant
+     * @throws PartInitException problem loading bundles
+     */
+    private void loadBundle(IEditorSite site, IResource resource, 
+            String lang, String country, String variant)
+            throws PartInitException {
+        if (resource != null && resource instanceof IFile) {
+            Bundle bundle = new Bundle();
+            Locale locale = null;
+            if (lang != null && country != null && variant != null) {
+                locale = new Locale(lang, country, variant);
+            } else if (lang != null && country != null) {
+                locale = new Locale(lang, country);
+            } else if (lang != null) {
+                locale = new Locale(lang);
+            }
+            bundle.setLocale(locale);
+            if (locale != null) {
+                bundle.setTitle(locale.getDisplayName());
+            } else {
+                bundle.setTitle(ResourceBundlePlugin.getResourceString(
+                        "editor.default"));
+            }
+            
+            IEditorInput newEditorInput = 
+                    new FileEditorInput((IFile) resource);
+            TextEditor textEditor = null;
+            try {
+                // Use PropertiesFileEditor if available
+                textEditor = (TextEditor) Class.forName(
+                        PROPERTIES_EDITOR_CLASS_NAME).newInstance();
+            } catch (Exception e) {
+                // Use default editor otherwise
+                textEditor = new TextEditor();
+            }
+            textEditor.init(site, newEditorInput);
+            bundle.setEditor(textEditor);
+            bundles.addBundle(bundle);
+        }
+    }
+    
     /**
      * Sets the bundle path and file name for the selected file.  
      * @param file <code>IFile</code>
