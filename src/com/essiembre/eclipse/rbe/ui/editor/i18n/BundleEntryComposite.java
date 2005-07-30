ec4f18be404f2cfd9f94c4fda685b2f57c0fc2d7
essiembre <essiembre>
2005-07-30 04:31:52 +0000

    Duplicate values can now be reported.

--- src/com/essiembre/eclipse/rbe/ui/editor/i18n/BundleEntryComposite.java
+++ src/com/essiembre/eclipse/rbe/ui/editor/i18n/BundleEntryComposite.java
@@ -20,8 +20,10 @@
  */
 package com.essiembre.eclipse.rbe.ui.editor.i18n;
 
+import java.util.Iterator;
 import java.util.Locale;
 
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.resource.ImageRegistry;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.FocusEvent;
@@ -32,6 +34,7 @@ import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.TraverseEvent;
 import org.eclipse.swt.events.TraverseListener;
+import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridData;
@@ -44,6 +47,7 @@ import org.eclipse.ui.texteditor.ITextEditor;
 
 import com.essiembre.eclipse.rbe.model.bundle.BundleEntry;
 import com.essiembre.eclipse.rbe.model.bundle.BundleGroup;
+import com.essiembre.eclipse.rbe.model.bundle.visitors.DuplicateValuesVisitor;
 import com.essiembre.eclipse.rbe.ui.RBEPlugin;
 import com.essiembre.eclipse.rbe.ui.UIUtils;
 import com.essiembre.eclipse.rbe.ui.editor.ResourceBundleEditor;
@@ -62,16 +66,21 @@ public class BundleEntryComposite extends Composite {
     private final Locale locale;
     private final Font boldFont;
     private final Font smallFont;
-
+    private final Color yellowColor = UIUtils.getSystemColor(SWT.COLOR_YELLOW);
+    
     private ImageRegistry imageRegistry = new ImageRegistry();
 
     private Text textBox;
     private Button commentedCheckbox;
     private Button gotoButton;
+    private Button duplButton;
+    private Button simButton;
     
     private String activeKey;
     private String textBeforeUpdate;
 
+    private DuplicateValuesVisitor duplVisitor;
+    
     
     /**
      * Constructor.
@@ -151,24 +160,48 @@ public class BundleEntryComposite extends Composite {
             commentedCheckbox.setEnabled(!sourceEditor.isReadOnly());
             textBox.setEnabled(!sourceEditor.isReadOnly());
             gotoButton.setEnabled(true);
+            if (RBEPreferences.getReportDuplicateValues()) {
+                findDuplicates(bundleEntry);
+            }
+            if (RBEPreferences.getReportSimilarValues()) {
+                if (RBEPreferences.getReportSimilarValuesLevensthein()) {
+                    
+                } else {
+                    
+                }
+            }
+            
         } else {
             commentedCheckbox.setSelection(false);
             commentedCheckbox.setEnabled(false);
             textBox.setText("");
             textBox.setEnabled(false);
             gotoButton.setEnabled(false);
+            duplButton.setVisible(false);
+            simButton.setVisible(false);
         }
         resetCommented();
     }
         
+    private void findDuplicates(BundleEntry bundleEntry) {
+        BundleGroup bundleGroup = resourceManager.getBundleGroup();
+        if (duplVisitor == null) {
+            duplVisitor = new DuplicateValuesVisitor();
+        }
+        duplVisitor.clear();
+        bundleGroup.getBundle(locale).accept(duplVisitor, bundleEntry);
+        duplButton.setVisible(duplVisitor.getDuplicates().size() > 0);
+    }
+    
+    
     /**
      * Creates the text field label, icon, and commented check box.
      */
     private void createLabelRow() {
         Composite labelComposite = new Composite(this, SWT.NONE);
         GridLayout gridLayout = new GridLayout();
-        gridLayout.numColumns = 4;
-        gridLayout.horizontalSpacing = 0;
+        gridLayout.numColumns = 6;
+        gridLayout.horizontalSpacing = 5;
         gridLayout.verticalSpacing = 0;
         gridLayout.marginWidth = 0;
         gridLayout.marginHeight = 0;
@@ -176,6 +209,74 @@ public class BundleEntryComposite extends Composite {
         labelComposite.setLayoutData(
                 new GridData(GridData.FILL_HORIZONTAL));
 
+        // Locale text
+        Label txtLabel = new Label(labelComposite, SWT.NONE);
+        txtLabel.setText(" " + UIUtils.getDisplayName(locale) + " ");
+        txtLabel.setFont(boldFont);
+
+        GridData gridData = new GridData();
+
+        // Similar button
+        gridData = new GridData();
+        gridData.horizontalAlignment = GridData.END;
+        simButton = new Button(labelComposite, SWT.PUSH | SWT.FLAT);
+        simButton.setImage(RBEPlugin.getImageDescriptor("similar.gif").createImage());
+        simButton.setLayoutData(gridData);
+        simButton.setVisible(false);
+        simButton.setToolTipText(
+                "Similar value(s) found. Click for details");
+
+        // Duplicate button
+        gridData.horizontalAlignment = GridData.END;
+        gridData.grabExcessHorizontalSpace = true;
+        duplButton = new Button(labelComposite, SWT.PUSH | SWT.FLAT);
+        duplButton.setImage(RBEPlugin.getImageDescriptor("duplicate.gif").createImage());
+        duplButton.setLayoutData(gridData);
+        duplButton.setVisible(false);
+        duplButton.setToolTipText(
+                "Duplicate value(s) found. Click for details");
+        duplButton.addSelectionListener(new SelectionAdapter() {
+            public void widgetSelected(SelectionEvent event) {
+                String msg = "Below are keys having the same value has key \""
+                        + activeKey + "\" within the locale \""
+                        + locale.getDisplayName() + "\":\n\n";
+                for (Iterator iter = duplVisitor.getDuplicates().iterator();
+                        iter.hasNext();) {
+                    msg += "        " + ((BundleEntry) iter.next()).getKey()
+                        + "\n";
+                }
+                MessageDialog.openInformation(
+                        getShell(), "Duplicate value(s) found.", msg); 
+            }
+        });
+        
+        // Commented checkbox
+        gridData = new GridData();
+        gridData.horizontalAlignment = GridData.END;
+        //gridData.grabExcessHorizontalSpace = true;
+        commentedCheckbox = new Button(
+                labelComposite, SWT.CHECK);
+        commentedCheckbox.setText("#");//TODO translate
+        commentedCheckbox.setFont(smallFont);
+        commentedCheckbox.setLayoutData(gridData);
+        commentedCheckbox.addSelectionListener(new SelectionAdapter() {
+            public void widgetSelected(SelectionEvent event) {
+                resetCommented();
+                updateBundleOnChanges();
+            }
+        });
+        commentedCheckbox.setEnabled(false);
+        
+        // Country flag
+        gridData = new GridData();
+        gridData.horizontalAlignment = GridData.END;
+        Label imgLabel = new Label(labelComposite, SWT.NONE);
+        imgLabel.setLayoutData(gridData);
+        imgLabel.setImage(loadCountryIcon(locale));
+
+        // Goto button
+        gridData = new GridData();
+        gridData.horizontalAlignment = GridData.END;
         gotoButton = new Button(
                 labelComposite, SWT.ARROW | SWT.RIGHT);
         gotoButton.setToolTipText(
@@ -192,34 +293,7 @@ public class BundleEntryComposite extends Composite {
                 }
             }
         });
-        Label txtLabel = new Label(labelComposite, SWT.NONE);
-        txtLabel.setText(" " + UIUtils.getDisplayName(locale) + " ");
-        txtLabel.setFont(boldFont);
-
-        GridData gridData = new GridData();
-        gridData.horizontalAlignment = GridData.END;
-        gridData.grabExcessHorizontalSpace = true;
-        commentedCheckbox = new Button(
-                labelComposite, SWT.CHECK);
-        commentedCheckbox.setText("# commented" + " ");//TODO translate
-        commentedCheckbox.setFont(smallFont);
-        commentedCheckbox.setLayoutData(gridData);
-        commentedCheckbox.addSelectionListener(new SelectionAdapter() {
-            public void widgetSelected(SelectionEvent event) {
-                resetCommented();
-                updateBundleOnChanges();
-            }
-        });
-        commentedCheckbox.setEnabled(false);
-        
-        gridData = new GridData();
-        gridData.horizontalAlignment = GridData.END;
-        //gridData.grabExcessHorizontalSpace = true;
-
-        Label imgLabel = new Label(labelComposite, SWT.NONE);
-        imgLabel.setLayoutData(gridData);
-        imgLabel.setImage(loadCountryIcon(locale));
-
+        gotoButton.setLayoutData(gridData);
     }
     
     /**
